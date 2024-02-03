f8ed84a7bf47e5a3dec631b5c4f3eb952188ca2c
Hagop Almadjian <hagop1isaboss@hotmail.com>
2024-02-02 16:16:22 -0800

    Quotes-project

--- quotes.js
+++ quotes.js
@@ -26,33 +26,42 @@ document.querySelector('figure>button').addEventListener('click', (e) => {
   document.querySelector('iframe')
   // switch statement backgroung chg.
 
+  const mainQuote = document.querySelector('main h2')
+
   switch (quotes[get_random_quote]) {
     case 'The greatest glory in living lies not in never falling, but in rising every time we fall. <br><em>- Nelson <span class=orange>Mandela</em></span>':
       document.body.style.backgroundColor = 'lightcyan'
+      mainQuote.style.color = 'red'
       break
 
     case 'The way to get started is to quit talking and begin doing. <br><em><span class=disney>- Walt Disney</em></span>':
       document.body.style.backgroundColor = 'limegreen'
+      mainQuote.style.color = 'red'
       break
 
     case "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. <br><em><span class=steve>- Steve Jobs</em></span>":
       document.body.style.backgroundColor = 'darkgrey'
+      mainQuote.style.color = 'blue'
       break
 
     case 'If life were predictable it would cease to be life, and be without flavor. <br><em><span class=elenor>- Eleanor Roosevelt</em></span>':
       document.body.style.backgroundColor = 'salmon'
+      mainQuote.style.color = ' black'
       break
 
     case 'If life were predictable it would cease to be life, and be without flavor. <br><em><span class=elenor>- Eleanor Roosevelt</em></span>':
       document.body.style.backgroundColor = 'salmon'
+      mainQuote.style.color = 'black'
       break
 
     case "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. <br><em><span class=oprah>- Oprah Winfrey</em></span>":
       document.body.style.backgroundColor = 'cyan'
+      mainQuote.style.color = 'black'
       break
 
     case 'What you get by achieving your goals is not as important as what you become by achieving your goals.<br><em><span class=xig>- Zig Ziglar</em></span>':
       document.body.style.backgroundColor = '#ccc'
+      mainQuote.style.color = 'orange'
       break
 
     case 'If  you set your goals ridiculously high and it’s a failure, you will fail above everyone else’s success.” <br><em><span class=James>– James Cameron</em></span>':
@@ -64,10 +73,12 @@ document.querySelector('figure>button').addEventListener('click', (e) => {
 
     case 'Find a person who is as successful as you’d like to be, ask them what to do, do it and work hard. <br><em><span class=tate>- Andrew Tate</em></span>':
       document.body.style.backgroundColor = 'lightblue'
+      mainQuote.style.color = '#fff'
       break
 
     case 'Do the impossible and you’ll never doubt yourself ever again. <br><em><span class=tate>- Andrew Tate</em></span>':
       document.body.style.backgroundColor = 'wheat'
+      mainQuote.style.color = 'limegreen'
       break
   }
 })
