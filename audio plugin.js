9d196ef4ef421e0196858b48d3f5aca40809dca3
Jack Almadjian <81959688+Hagop420@users.noreply.github.com>
2024-02-06 01:20:14 -0800

    Delete audio plugin.js

--- audio plugin.js
+++
@@ -1,7 +0,0 @@
-const audioPlayWhenButtonIsClicked = new Audio()
-
-document.querySelector('button').addEventListener('click', () => {
-   audioPlayWhenButtonIsClicked.src = 'https://www.fesliyanstudios.com/play-mp3/387'
- 
-   audioPlayWhenButtonIsClicked.play()
-})
